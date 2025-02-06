d98c8120c91c86ba8304d256f56358dd69c48b75
deepspace1 <harshkumar170604@gmail.com>
2025-02-07 01:10:53 +0530

    optimized images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,6 +3,16 @@ import "../stylesheet/carousel.css";
 
 const Carousel = ({images}) => {
 
+  const optimizeImageUrl = (url) => {
+    return url.replace("/upload/", "/upload/f_auto,q_auto:low,w_800,h_600,c_fit,fl_progressive/");
+  };
+  
+  const optimizedImages = images.map(img => ({
+    ...img,
+    image: optimizeImageUrl(img.image),
+  }));
+  
+
   const [current, setcurrent] = useState(0);
 
   const slide_left = () => {
@@ -32,7 +42,7 @@ const Carousel = ({images}) => {
   return (
     <div className='carousel'>
       <div className='carousel_wrapper'>
-        {images.map((image, index)=>{
+        {optimizedImages.map((image, index)=>{
           return (
           <div key={index} className={index==current ? 'carousel_card carousel_card-active' : 'carousel_card'}>
             <img className='card_image' src={image.image} />
@@ -54,7 +64,6 @@ const Carousel = ({images}) => {
             return (
               <div key={index}
                 className={index==current ? "pagination_dot pagination_dot-active" : "pagination_dot"}
-                onClick={() => setcurrent(index)}
               ></div>
             );
           })}
